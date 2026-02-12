fbb19165cd853ce06a67d4f34232a5a1a9095fc9
Rahul Joshi <joshirahul7040@gmail.com>
2026-02-12 15:00:29 +0530

    update and deleted endpoints with validation

--- patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -2,12 +2,16 @@ package com.pm.patient_service.controller;
 
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
 import com.pm.patient_service.service.PatientService;
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -25,8 +29,24 @@ public class PatientController {
     }
 
     @PostMapping("/create-patient")
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDTO> createPatient(
+            @Validated({Default.class, CreatePatientValidationGroup.class})
+            @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+    @PutMapping("/update-patient/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(
+            @PathVariable UUID id,
+            @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+    
+    @DeleteMapping("/delete-patient/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id){
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -1,6 +1,7 @@
 package com.pm.patient_service.dto;
 
 
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -21,13 +22,10 @@ public class PatientRequestDTO {
     @NotBlank(message = "Date of birth is required")
     private String dateOfBirth;
 
-//    @NotBlank(groups = CreatePatientValidationGroup.class, message =
-//            "Registered date is required")
-//    private String registeredDate;
-
-    @NotBlank(message =
+    @NotBlank(groups = CreatePatientValidationGroup.class, message =
             "Registered date is required")
     private String registeredDate;
+
 
     public @NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String getName() {
         return name;
